3bbd9bd11e3c5a8ab415d1f3f292097ea41095f3
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-02-18 19:53:10 +0530

    update

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./style.css";
+import "@/assets/scss/index.scss";
 
 let app = createApp(App);
 
--- client/tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,vue,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
