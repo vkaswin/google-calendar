1037817ae612454e83717708a1fee18ad11bce0d
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-02-24 23:25:49 +0530

    update

--- client/src/components/Calendar/WeekCalendar.vue
+++ client/src/components/Calendar/WeekCalendar.vue
@@ -17,30 +17,102 @@ let todayDate = new Date();
 let columns = computed(() => (view.value === "week" ? 7 : 1));
 
 let times = [
-  "12 PM",
-  "1 AM",
-  "2 AM",
-  "3 AM",
-  "4 AM",
-  "5 AM",
-  "6 AM",
-  "7 AM",
-  "8 AM",
-  "9 AM",
-  "10 AM",
-  "11 AM",
-  "12 AM",
-  "1 PM",
-  "2 PM",
-  "3 PM",
-  "4 PM",
-  "5 PM",
-  "6 PM",
-  "7 PM",
-  "8 PM",
-  "9 PM",
-  "10 PM",
-  "11 PM",
+  {
+    time: "12 PM - 1 AM",
+    label: "12 PM",
+  },
+  {
+    time: "1 AM - 2 AM",
+    label: "1 AM",
+  },
+  {
+    time: "2 AM - 3 AM",
+    label: "2 AM",
+  },
+  {
+    time: "3 AM - 4 AM",
+    label: "3 AM",
+  },
+  {
+    time: "4 AM - 5 AM",
+    label: "4 AM",
+  },
+  {
+    time: "5 AM - 6 AM",
+    label: "5 AM",
+  },
+  {
+    time: "6 AM - 7 AM",
+    label: "6 AM",
+  },
+  {
+    time: "7 AM - 8 AM",
+    label: "7 AM",
+  },
+  {
+    time: "8 AM - 9 AM",
+    label: "8 AM",
+  },
+  {
+    time: "9 AM - 10 AM",
+    label: "9 AM",
+  },
+  {
+    time: "10 AM - 11 AM",
+    label: "10 AM",
+  },
+  {
+    time: "11 AM - 12 AM",
+    label: "11 AM",
+  },
+  {
+    time: "12 AM - 1 PM",
+    label: "12 AM",
+  },
+  {
+    time: "1 PM - 2 PM",
+    label: "1 PM",
+  },
+  {
+    time: "2 PM - 3 PM",
+    label: "2 PM",
+  },
+  {
+    time: "3 PM - 4 PM",
+    label: "3 PM",
+  },
+  {
+    time: "4 PM - 5 PM",
+    label: "4 PM",
+  },
+  {
+    time: "5 PM - 6 PM",
+    label: "5 PM",
+  },
+  {
+    time: "6 PM - 7 PM",
+    label: "6 PM",
+  },
+  {
+    time: "7 PM - 9 PM",
+    label: "7 PM",
+  },
+  {
+    time: "8 PM - 9 PM",
+    label: "8 PM",
+  },
+  {
+    time: "9 PM - 10 PM",
+    label: "9 PM",
+  },
+  {
+    time: "10 PM - 11 PM",
+    label: "10 PM",
+  },
+  {
+    time: "11 PM - 12 PM",
+    label: "11 PM",
+  },
 ];
 
 let dates = computed(() => {
@@ -66,20 +138,12 @@ let dates = computed(() => {
   return dates;
 });
 
-let handleTime = (row?: number, column?: number) => {
-  if (typeof row === "number" && typeof column === "number") {
-    let time: string;
-    let date = dates.value[column];
-
-    if (row === times.length - 1) {
-      time = `${times[row]} - ${times[0]}`;
-    } else {
-      time = `${times[row]} - ${times[row + 1]}`;
-    }
+let handleEvent = (date?: Date, time?: string) => {
+  if (date && time) {
     console.log(
-      "🚀 ~ file: WeekCalendar.vue:72 ~ handleTime ~ time, date",
-      time,
-      date
+      "🚀 ~ file: WeekCalendar.vue:72 ~ handleTime ~ date, time",
+      date,
+      time
     );
   } else {
     console.log("close popup");
@@ -108,17 +172,17 @@ let handleTime = (row?: number, column?: number) => {
     <div :class="styles.time_section">
       <div
         :class="styles.time_container"
-        v-for="(time, row) in times"
-        :key="row"
+        v-for="({ label, time }, index) in times"
+        :key="index"
       >
         <div :class="styles.time">
-          <span v-if="row > 0">{{ time }}</span>
+          <span v-if="index !== 0">{{ label }}</span>
         </div>
         <div
           :class="styles.date"
-          v-for="(_, column) in columns"
-          :key="column"
-          @click="handleTime(row, column)"
+          v-for="index in columns"
+          :key="index"
+          @click="handleEvent(dates[index - 1], time)"
         ></div>
       </div>
     </div>
