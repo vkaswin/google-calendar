1e0e731028b38dd4ecdc4f051f6ede9f63518b75
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-03-03 13:01:43 +0530

    update

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/google-calendar/" : "/",
-})
+  publicPath: process.env.NODE_ENV === "production" ? "/calendar-app/" : "/",
+});
